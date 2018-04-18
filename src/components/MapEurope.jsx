d636ad5c9009705ba734c37b389fac7b9705be2b
Truls Aagaard <truls.aa@gmail.com>
2018-04-18 20:39:18 +0200

    Cleanup in MapEurope

--- src/components/MapEurope.jsx
+++ src/components/MapEurope.jsx
@@ -6,7 +6,7 @@ import map from '../mapdata/world';
 class MapEurope extends React.Component {
   config = {
     title: {
-      text: 'Map of many contries',
+      text: 'Map of many countries',
     },
 
     legend: {
@@ -15,58 +15,12 @@ class MapEurope extends React.Component {
 
     plotOptions: {
       map: {
-        allAreas: true,
-        joinBy: ['iso-a2', 'code'],
-        dataLabels: {
-          enabled: true,
-          color: 'white',
-          style: {
-            fontWeight: 'bold',
-          },
-        },
         mapData: map,
-        tooltip: {
-          headerFormat: '',
-          pointFormat: '{point.name}: <b>{series.name}</b>',
-        },
       },
     },
 
     series: [
-      {
-        name: 'UTC',
-        data: ['IE', 'IS', 'GB', 'PT'].map(code => ({ code })),
-      },
-      {
-        name: 'UTC + 1',
-        data: [
-          'NO',
-          'SE',
-          'DK',
-          'DE',
-          'NL',
-          'BE',
-          'LU',
-          'ES',
-          'FR',
-          'PL',
-          'CZ',
-          'AT',
-          'CH',
-          'LI',
-          'SK',
-          'HU',
-          'SI',
-          'IT',
-          'SM',
-          'HR',
-          'BA',
-          'YF',
-          'ME',
-          'AL',
-          'MK',
-        ].map(code => ({ code })),
-      },
+      { },
     ],
   };
   render() {
